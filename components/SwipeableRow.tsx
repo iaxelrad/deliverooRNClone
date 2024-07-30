3298102d3733c431ecdcf1638ef82eb92f1e3efd
Itamar <iaxelrad@gmail.com>
2024-07-30 08:26:46 +0300

    converted component to typescript

--- components/SwipeableRow.tsx
+++ components/SwipeableRow.tsx
@@ -1,18 +1,24 @@
 import { Ionicons } from '@expo/vector-icons';
-import React, { Component, PropsWithChildren } from 'react';
+import React, { Component, PropsWithChildren, useRef } from 'react';
 import { Animated, StyleSheet, I18nManager, View } from 'react-native';
 
 import { RectButton, Swipeable } from 'react-native-gesture-handler';
 
-export default class SwipeableRow extends Component<
-  PropsWithChildren<unknown & { onDelete: () => void }>
-> {
-  private renderRightActions = (
+interface SwipeableRowProps {
+  onDelete: () => void;
+}
+
+const SwipeableRow: React.FC<React.PropsWithChildren<SwipeableRowProps>> = ({
+  onDelete,
+  children,
+}) => {
+  const swipeableRow = useRef<Swipeable>(null);
+
+  const renderRightActions = (
     _progress: Animated.AnimatedInterpolation<number>
   ) => {
     return (
-      <RectButton style={styles.rightAction} onPress={this.close}>
-        {/* Change it to some icons */}
+      <RectButton style={styles.rightAction} onPress={close}>
         <Ionicons
           name="trash-outline"
           size={24}
@@ -23,31 +29,24 @@ export default class SwipeableRow extends Component<
     );
   };
 
-  private swipeableRow?: Swipeable;
+  const close = () => {
+    swipeableRow.current?.close();
+    onDelete();
+  };
 
-  private updateRef = (ref: Swipeable) => {
-    this.swipeableRow = ref;
-  };
-  private close = () => {
-    this.swipeableRow?.close();
-    this.props.onDelete();
-  };
-  render() {
-    const { children } = this.props;
-    return (
-      <Swipeable
-        ref={this.updateRef}
-        friction={2}
-        leftThreshold={80}
-        enableTrackpadTwoFingerGesture
-        rightThreshold={40}
-        renderRightActions={this.renderRightActions}
-      >
-        {children}
-      </Swipeable>
-    );
-  }
-}
+  return (
+    <Swipeable
+      ref={swipeableRow}
+      friction={2}
+      leftThreshold={80}
+      enableTrackpadTwoFingerGesture
+      rightThreshold={40}
+      renderRightActions={renderRightActions}
+    >
+      {children}
+    </Swipeable>
+  );
+};
 
 const styles = StyleSheet.create({
   rightAction: {
@@ -58,3 +57,60 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
   },
 });
+
+export default SwipeableRow;
+
+// export default class SwipeableRow extends Component<
+//   PropsWithChildren<unknown & { onDelete: () => void }>
+// > {
+//   private renderRightActions = (
+//     _progress: Animated.AnimatedInterpolation<number>
+//   ) => {
+//     return (
+//       <RectButton style={styles.rightAction} onPress={this.close}>
+//         {/* Change it to some icons */}
+//         <Ionicons
+//           name="trash-outline"
+//           size={24}
+//           color="#fff"
+//           style={{ marginRight: 10 }}
+//         />
+//       </RectButton>
+//     );
+//   };
+
+//   private swipeableRow?: Swipeable;
+
+//   private updateRef = (ref: Swipeable) => {
+//     this.swipeableRow = ref;
+//   };
+//   private close = () => {
+//     this.swipeableRow?.close();
+//     this.props.onDelete();
+//   };
+//   render() {
+//     const { children } = this.props;
+//     return (
+//       <Swipeable
+//         ref={this.updateRef}
+//         friction={2}
+//         leftThreshold={80}
+//         enableTrackpadTwoFingerGesture
+//         rightThreshold={40}
+//         renderRightActions={this.renderRightActions}
+//       >
+//         {children}
+//       </Swipeable>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   rightAction: {
+//     alignItems: 'center',
+//     flexDirection: I18nManager.isRTL ? 'row-reverse' : 'row',
+//     backgroundColor: '#dd2c00',
+//     flex: 1,
+//     justifyContent: 'flex-end',
+//   },
+// });
